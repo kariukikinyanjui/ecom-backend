e6cc403f3d8a44d043b87166b664fa494fc74ba9
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-02-26 02:12:22 +0300

    feat: define the User Model

--- services/auth-service/src/users/models.py
+++ services/auth-service/src/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_staff = models.BooleanField(default=False)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        # Isolated table name for the auth service
+        db_table = "auth_users"
