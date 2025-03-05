865fb36a019a396e4d6fae50ca489285bb50cca2
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-05 23:36:03 +0300

    feat: added documentation and a __str__ method to define how instances of the User model should be displayed

--- services/auth-service/src/users/models.py
+++ services/auth-service/src/users/models.py
@@ -3,9 +3,24 @@ from django.db import models
 
 
 class User(AbstractUser):
+    '''
+    Custom User model extending Django's build-in AbstractUser.
+    This model is designed to provide additional flexibility for user management
+    within the authentication service. It overries or adds fields as needed
+    while maintaining compatibility with Django's authentication system.
+    '''
     is_staff = models.BooleanField(default=False)
     last_login = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         # Isolated table name for the auth service
         db_table = "auth_users"
+
+    def __str__(self):
+        '''
+        String representation of the User object.
+
+        Returns:
+            str: The username of the user.
+        '''
+        return self.username
