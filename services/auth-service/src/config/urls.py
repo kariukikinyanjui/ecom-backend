cd1638fd50e3c47aa75c432a8ea3a363c73f734b
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-06 10:12:45 +0300

    feat: added `permission_class`

--- services/auth-service/src/config/urls.py
+++ services/auth-service/src/config/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Auth Service API",
         default_version='v1',
+        description="Authentication endpoints",
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
