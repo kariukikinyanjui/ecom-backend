a9ee727d489d65d607d8f00f86cafbf729229020
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-03-07 18:31:09 +0300

    feat: added bulk views

--- services/product-service/src/products/views.py
+++ services/product-service/src/products/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
